4961863c574f237256b81a222a4bfd323e6d66bc
yanxel18 <yanxel18@gmail.com>
2023-04-19 15:53:44 +0900

    update

--- src/components/c-areatotal-table/c-areatotal-table.component.ts
+++ src/components/c-areatotal-table/c-areatotal-table.component.ts
@@ -1,13 +1,16 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
-import { Observable} from 'rxjs';
-import { IPerArea, IPerAreaTotalStatistics, ITotalArea } from 'src/models/viewboard-model';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
- 
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
+import {
+  IPerArea,
+  IPerAreaTotalStatistics,
+  ITotalArea,
+} from 'src/models/viewboard-model';
+
 @Component({
   selector: 'app-c-areatotal-table',
   templateUrl: './c-areatotal-table.component.html',
@@ -16,23 +19,29 @@ export interface PeriodicElement {
 export class CAreatotalTableComponent implements OnChanges {
   @Input() DataSource!: IPerAreaTotalStatistics | null;
   @Output() PieDataSource = new EventEmitter<ITotalArea[] | null>();
-  tableDatasource!:  IPerArea[];
-  displayedColumns: string[] = ['bldg', 'area', 'workerIn', 'workerInTotal', 'percent'];
+  tableDatasource!: IPerArea[];
+  displayedColumns: string[] = [
+    'bldg',
+    'area',
+    'workerIn',
+    'workerInTotal',
+    'percent',
+  ];
 
   constructor() {}
 
   ngOnChanges(): void {
-      this.tableDatasource = this.DataSource ? this.DataSource.PerArea : []
+    this.tableDatasource = this.DataSource ? this.DataSource.PerArea : [];
   }
 
   getTableRow(element: IPerArea): void {
-    const rawData : ITotalArea[] = [];
-    const newTotalArea: ITotalArea =  {
-        total: element.workerInTotal,
-        percent: element.percent,
-        workerIn: element.workerIn,
-      }
-      rawData.push(newTotalArea)
-    this.PieDataSource.emit(rawData)
+    const rawData: ITotalArea[] = [];
+    const newTotalArea: ITotalArea = {
+      total: element.workerInTotal,
+      percent: element.percent,
+      workerIn: element.workerIn,
+    };
+    rawData.push(newTotalArea);
+    this.PieDataSource.emit(rawData);
   }
 }
